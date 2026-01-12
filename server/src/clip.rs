50d6b110cf35cb9e95623c0e62b8787e4cb6e06d
Florian Dobisch <Florian.Dobisch@mercedes-benz.com>
2026-01-12 17:30:12 +0100

    chore: update embed_anything dependency to version 0.6.7 and adjust model initialization

--- server/src/clip.rs
+++ server/src/clip.rs
@@ -15,7 +15,7 @@ pub async fn clip<C>(state: &AppState<C>, input: String) -> Vec<f32>
 
 pub async fn init_embedder() -> Result<Embedder, Box<dyn std::error::Error + Send + Sync>> {
     let clip_embedder =
-        Embedder::from_pretrained_hf("Clip", "openai/clip-vit-large-patch14", None, None, None)?;
+        Embedder::from_pretrained_hf("Clip", Some("openai/clip-vit-large-patch14"), None, None)?;
     info!("Embedder initialized");
     Ok(clip_embedder)
 }
